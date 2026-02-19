a361b5ee99bbe97eeff1c309d35b91efdaacaf6d
Mateusz <0xleodev@Mateuszs-MacBook-Air.local>
2026-02-20 00:06:42 +0100

    content

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import logo from "./media/logo.svg";
 
-function App() {
-  return <div className="App">Imbryk</div>;
+export default function App() {
+  return (
+    <div className="page">
+      <div className="header">
+        <img src={logo} alt="Fundacja Imbryk" className="logo" />
+      </div>
+      <div className="content">
+        <div className="message">
+          <h2>Witamy na oficjalnej stronie Fundacji Imbryk Ikigai!</h2>
+          <p>
+            Strona jest obecnie w trakcie przygotowywania. <br /> Wkrótce
+            wrócimy z pełną wersją.
+          </p>
+        </div>
+      </div>
+      <div className="footer"> </div>
+    </div>
+  );
 }
-
-export default App;
